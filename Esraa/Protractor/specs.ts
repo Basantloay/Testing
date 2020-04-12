4879b67337b844607d738caf27465f52937aa3bb
Esraa999 <55926251+Esraa999@users.noreply.github.com>
2020-04-12 02:57:21 +0200

    Add files via upload

--- Esraa/Protractor/specs.ts
+++ Esraa/Protractor/specs.ts
@@ -1,36 +1,99 @@
-import { by, browser, element } from 'protractor'
+var protractor_1 = require("protractor");
 
-// let loginPage = require('../pages/loginPage');
+/**
+ * This File tests HomePgae and ViewArtist
+ */
 
-describe('incorrect password', function () {
-  // beforeEach(function(){
-  //     browser.get('http://3.137.69.49/account.mayestro/login');
-  // });
-  it('should have error message', async function () {
-    browser.get('http://3.137.69.49/account.mayestro/login')
-    browser.sleep(3000)
-    element(by.xpath("//input[@name='email']")).sendKeys('heloo')
-    element(by.xpath("//input[@name='password']")).sendKeys('123456789')
-    element(by.xpath("//button[@type='submit']")).click()
-    expect(await element(by.xpath("//p[@class='col-12 mb-0 text-white']")).getText()).toContain('Incorrect username or password.')
+describe('Home Tests', () => {
+    
+/**
+ * This Describe function tests HomePage
+ * @returns several test cases
+ */
 
-    // loginPage.get('http://3.137.69.49/account.mayestro/login');
-    // loginPage.enterEmail('heloo');
-    // loginPage.enterPassword('12345678');
-    // loginPage.clickLogin();
-    // expect(await element(by.xpath("//p[@class='col-12 mb-0 text-white']")).getText()).toContain('Incorrect username or password.');
-  })
-})
+        protractor_1.browser.get('http://localhost:4200/mayestro/home');
+        it('Test Home Text', () => {
+
+            var li = protractor_1.element(protractor_1.by.xpath("//a[contains(.,'Home')]"));
+            expect(li.getText()).toContain('Home');
+            
+        });
+
+        it('Test Search Text', () => {
+    
+            var li = protractor_1.element(protractor_1.by.xpath("//a[contains(.,'Search')]"));
+            expect(li.getText()).toContain('Search');
+        });
+        it('Test Library Text', () => {
+        
+            var li = protractor_1.element(protractor_1.by.xpath("//a[contains(.,'Your Library')]"));
+            expect(li.getText()).toContain('Your Library');
+        });
+
+        it('Test Playlist Text', () => {
+            
+            var li = protractor_1.element(protractor_1.by.xpath("//a[contains(.,'Create PlayList')]"));
+            expect(li.getText()).toContain('Create PlayList');
+        });
+
+        it('Test Liked Songs Text', () => {
+        
+            var li = protractor_1.element(protractor_1.by.xpath("//a[contains(.,'Liked Songs')]"));
+            expect(li.getText()).toContain('Liked Songs');
+        });
+
+        it ('test that when user click home button, it returns home url ',() => {
+            protractor_1.browser.waitForAngularEnabled(false);
+            var li = protractor_1.element(protractor_1.by.xpath("//a[contains(.,'Home')]")).click();
+            protractor_1.browser.waitForAngularEnabled(true);
+            expect(protractor_1.browser.getCurrentUrl()).toBe('http://localhost:4200/mayestro/home');
+        });
+
+       
+        it('Test Home Scroll Down',() => {
+            protractor_1.browser.executeScript('window.scrollTo(0,10000);').then(function(){
+                console.log('++++++SCROLLED Down+++++');
+            });
+        });
 
 
+        it('Test Home Scroll Up',() => {
+            protractor_1.browser.executeScript('window.scrollTo(0,0);').then(function(){
+                console.log('++++++SCROLLED UP+++++');
+            });
+        });
 
-// import { browser, element, by } from "protractor";
 
-// describe("Calculator test", function(){
+        describe('View artist', () => {
+     
+    
+            it('Test Artist-1 click', () => {
+                protractor_1.browser.get('http://localhost:4200/mayestro/home');
+                protractor_1.browser.waitForAngularEnabled(false);
+                var Artist1 = protractor_1.element(protractor_1.by.xpath("//a[@href='/mayestro/artist/3xl0OvcSlc9Mwe5ToaFtD3']")).click();
+                protractor_1.browser.waitForAngularEnabled(true);
+                protractor_1.browser.get('http://localhost:4200/mayestro/artist/3xl0OvcSlc9Mwe5ToaFtD3/overview');
+                let artisttitle=protractor_1.browser.element(protractor_1.by.xpath("//h1[@class='ArtistName']"))
+                expect(artisttitle.getText()).toEqual("Amr Diab");
+                
+    
+            });
+            it('Test Artist-2 click', () => {
+                protractor_1.browser.get('http://localhost:4200/mayestro/home'); 
+                protractor_1.browser.waitForAngularEnabled(false);
+                var Artist1 = protractor_1.element(protractor_1.by.xpath("//a[contains(.,'Tamer Hosni')]")).click();
+                protractor_1.browser.waitForAngularEnabled(true);
+                protractor_1.browser.get('http://localhost:4200/mayestro/artist/3xl0OvcSlc9Mwe5ToaFtD3/overview');
+                let artisttitle=protractor_1.browser.element(protractor_1.by.xpath("//h1[@class='ArtistName']"))
+                expect(artisttitle.getText()).toEqual("Tamer Hosni");
+            });
+            protractor_1.browser.sleep(6000);
+    
+          
+      });
 
-//     it("invoke calc app",function(){
-//         browser.get("https://juliemr.github.io/protractor-demo/");
-//         browser.sleep(3000);
-//     })
 
-// });
+        
+
+      
+});
