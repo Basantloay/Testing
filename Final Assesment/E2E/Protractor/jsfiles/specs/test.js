b0cbaeaad9ba78ec7e86196288d3acccb7273deb
basantloay <basantloay2012@gmail.com>
2020-05-27 21:48:07 +0200

    modify configurations for non-angular files

--- Final Assesment/E2E/Protractor/jsfiles/specs/test.js
+++ Final Assesment/E2E/Protractor/jsfiles/specs/test.js
@@ -5,14 +5,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const protractor_1 = require("protractor");
 // eslint-disable-next-line no-undef
 describe('Player Test ', function () {
-    // beforeEach(function () {
-    //  (global as any).isAngularSite(false)
-    // })
+    beforeEach(function () {
+        global.isAngularSite(false);
+    });
     it('Playlist ', function () {
         protractor_1.browser.get('https://open.spotify.com/');
-        protractor_1.browser.sleep(1000);
-        protractor_1.browser.click(protractor_1.element(protractor_1.by.className('_2221af4e93029bedeab751d04fab4b8b-scss _1edf52628d509e6baded2387f6267588-scss')));
+        protractor_1.browser.sleep(3000);
+        const login1 = protractor_1.element(protractor_1.by.className('_2221af4e93029bedeab751d04fab4b8b-scss _1edf52628d509e6baded2387f6267588-scss'));
+        login1.click();
         protractor_1.browser.sleep(2000);
     });
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NwZWNzL3Rlc3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxtQ0FBbUM7QUFDbkMsNkJBQTZCO0FBQzdCLDJDQUFzRTtBQUV0RSxvQ0FBb0M7QUFDcEMsUUFBUSxDQUFDLGNBQWMsRUFBRTtJQUN2QiwyQkFBMkI7SUFDM0Isd0NBQXdDO0lBQ3hDLEtBQUs7SUFFTCxFQUFFLENBQUMsV0FBVyxFQUFFO1FBQ2Qsb0JBQU8sQ0FBQyxHQUFHLENBQUMsMkJBQTJCLENBQUMsQ0FBQTtRQUN4QyxvQkFBTyxDQUFDLEtBQUssQ0FBQyxJQUFJLENBQUMsQ0FBQTtRQUNuQixvQkFBTyxDQUFDLEtBQUssQ0FBQyxvQkFBTyxDQUFDLGVBQUUsQ0FBQyxTQUFTLENBQUMsK0VBQStFLENBQUMsQ0FBQyxDQUFDLENBQUE7UUFDckgsb0JBQU8sQ0FBQyxLQUFLLENBQUMsSUFBSSxDQUFDLENBQUE7SUFDckIsQ0FBQyxDQUFDLENBQUE7QUFDSixDQUFDLENBQUMsQ0FBQSJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NwZWNzL3Rlc3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxtQ0FBbUM7QUFDbkMsNkJBQTZCO0FBQzdCLDJDQUFzRTtBQUV0RSxvQ0FBb0M7QUFDcEMsUUFBUSxDQUFDLGNBQWMsRUFBRTtJQUN2QixVQUFVLENBQUM7UUFDUixNQUFjLENBQUMsYUFBYSxDQUFDLEtBQUssQ0FBQyxDQUFBO0lBQ3JDLENBQUMsQ0FBQyxDQUFBO0lBRUgsRUFBRSxDQUFDLFdBQVcsRUFBRTtRQUNkLG9CQUFPLENBQUMsR0FBRyxDQUFDLDJCQUEyQixDQUFDLENBQUE7UUFDeEMsb0JBQU8sQ0FBQyxLQUFLLENBQUMsSUFBSSxDQUFDLENBQUE7UUFDbEIsTUFBTSxNQUFNLEdBQUcsb0JBQU8sQ0FBQyxlQUFFLENBQUMsU0FBUyxDQUFDLCtFQUErRSxDQUFDLENBQUMsQ0FBQTtRQUN0SCxNQUFNLENBQUMsS0FBSyxFQUFFLENBQUE7UUFDZCxvQkFBTyxDQUFDLEtBQUssQ0FBQyxJQUFJLENBQUMsQ0FBQTtJQUNyQixDQUFDLENBQUMsQ0FBQTtBQUNKLENBQUMsQ0FBQyxDQUFBIn0=
--- Final Assesment/E2E/Protractor/specs/test.ts
+++ Final Assesment/E2E/Protractor/specs/test.ts
@@ -4,14 +4,15 @@ import { browser, element, by, protractor, Browser } from 'protractor'
 
 // eslint-disable-next-line no-undef
 describe('Player Test ', function () {
-  // beforeEach(function () {
-  //  (global as any).isAngularSite(false)
-  // })
+  beforeEach(function () {
+    (global as any).isAngularSite(false)
+   })
 
   it('Playlist ', function () {
     browser.get('https://open.spotify.com/')
-    browser.sleep(1000)
-    browser.click(element(by.className('_2221af4e93029bedeab751d04fab4b8b-scss _1edf52628d509e6baded2387f6267588-scss')))
+    browser.sleep(3000)
+     const login1 = element(by.className('_2221af4e93029bedeab751d04fab4b8b-scss _1edf52628d509e6baded2387f6267588-scss'))
+    login1.click() 
     browser.sleep(2000)
   })
 })
